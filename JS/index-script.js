f749030be4b9dcd2b7a52018bfb85fbffa9808f6
Felix <felixfreidman@gmail.com>
2020-06-15 00:15:28 +0800

    Сделал почти весь Landing Page, остался только Date Picker доделать, добил страницу для регистрации, я молодец!

--- JS/index-script.js
+++ JS/index-script.js
@@ -1,31 +1,29 @@
-let indexMainBody = document.querySelector('.main');
+let mainSection = document.querySelector('.main');
 let signInButton = document.querySelector('.header_signin');
 let signUpButton = document.querySelector('.header_signup');
-let mainBodyBottomText = document.querySelector('.main-bottom_text');
-let roomAndCalendar = document.querySelector('.main-calendar');
-let signInSection = document.querySelector('.main-signin');
-let cross = document.querySelector('.window-close_cross')
+let mainSectionText = document.querySelector('.main-bottom_text');
+let calendarSection = document.querySelector('.main-calendar');
+let signInSection = document.querySelector('.signin');
+let signUpSection = document.querySelector('.signup');
 
-signInButton.addEventListener('click', () => {
-    indexMainBody.classList.toggle('main');
-    indexMainBody.classList.toggle('main_buttonclicked');
-    mainBodyBottomText.classList.toggle('hide');
-    roomAndCalendar.classList.toggle('hide');
+signInButton.addEventListener('click', ()=> {
+    mainSection.classList.toggle('main');
+    mainSection.classList.toggle('main_buttonclicked');
+    calendarSection.classList.toggle('hide');
     signInSection.classList.toggle('hide');
+    mainSectionText.classList.toggle('hide');
+    if(!signUpSection.classList.contains('hide')){
+        console.log(signUpSection.classList.contains('hide'));
+        signUpSection.classList.toggle('hide');
+    }
+});
+signUpButton.addEventListener('click', ()=> {
+    mainSection.classList.toggle('main');
+    mainSection.classList.toggle('main_buttonclicked');
+    calendarSection.classList.toggle('hide');
+    signUpSection.classList.toggle('hide');
+    mainSectionText.classList.toggle('hide');
 });
 
-cross.addEventListener('click', () => {
-    indexMainBody.classList.toggle('main');
-    indexMainBody.classList.toggle('main_buttonclicked');
-    mainBodyBottomText.classList.toggle('hide');
-    roomAndCalendar.classList.toggle('hide');
-    signInSection.classList.toggle('hide');
-})
 
 
-signUpButton.addEventListener('click', () => {
-    indexMainBody.classList.toggle('main');
-    indexMainBody.classList.toggle('main_buttonclicked');
-    mainBodyBottomText.classList.toggle('main-bottom_text');
-    mainBodyBottomText.classList.toggle('hide');
-});
